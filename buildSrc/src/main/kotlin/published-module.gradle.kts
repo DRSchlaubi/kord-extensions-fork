f9fd66d29e494e399f6386af24a4f811c92a8252
Michael Rittmeister <michael@rittmeister.in>
2024-08-25 21:26:55 +0200

    I don't even know anymore tbh

--- buildSrc/src/main/kotlin/published-module.gradle.kts
+++ buildSrc/src/main/kotlin/published-module.gradle.kts
@@ -10,69 +10,67 @@ val sourceJar: Task by tasks.getting
 val javadocJar: Task by tasks.getting
 //val dokkaJar: Task by tasks.getting
 
-afterEvaluate {
-	publishing {
-		repositories {
-			maven("artifactregistry://europe-west3-maven.pkg.dev/mik-music/mikbot") {
-				credentials {
-					username = "_json_key_base64"
-					password = System.getenv("GOOGLE_KEY")?.toByteArray()?.let {
-						Base64.getEncoder().encodeToString(it)
-					}
-				}
-
-				authentication {
-					create<BasicAuthentication>("basic")
+publishing {
+	repositories {
+		maven("artifactregistry://europe-west3-maven.pkg.dev/mik-music/mikbot") {
+			credentials {
+				username = "_json_key_base64"
+				password = System.getenv("GOOGLE_KEY")?.toByteArray()?.let {
+					Base64.getEncoder().encodeToString(it)
 				}
 			}
+
+			authentication {
+				create<BasicAuthentication>("basic")
+			}
 		}
+	}
 
-		publications {
-			create<MavenPublication>("maven") {
-				from(components.getByName("java"))
+	publications {
+		create<MavenPublication>("maven") {
+			from(components.getByName("java"))
 
-				artifact(sourceJar)
-				artifact(javadocJar)
+			artifact(sourceJar)
+			artifact(javadocJar)
 //                artifact(dokkaJar)
 
-				pom {
-					name.set(project.ext.get("pubName").toString())
-					description.set(project.ext.get("pubDesc").toString())
+			pom {
+				name.set(provider {project.ext.get("pubName").toString() })
+				description.set(provider { project.ext.get("pubDesc").toString() })
 
-					url.set("https://kordex.dev")
+				url.set("https://kordex.dev")
 
-					packaging = "jar"
+				packaging = "jar"
 
-					scm {
-						connection.set("scm:git:https://github.com/Kord-Extensions/kord-extensions.git")
-						developerConnection.set("scm:git:git@github.com:Kord-Extensions/kord-extensions.git")
-						url.set("https://github.com/Kord-Extensions/kord-extensions.git")
+				scm {
+					connection.set("scm:git:https://github.com/Kord-Extensions/kord-extensions.git")
+					developerConnection.set("scm:git:git@github.com:Kord-Extensions/kord-extensions.git")
+					url.set("https://github.com/Kord-Extensions/kord-extensions.git")
+				}
+
+				licenses {
+					license {
+						name.set("Mozilla Public License Version 2.0")
+						url.set("https://www.mozilla.org/en-US/MPL/2.0/")
 					}
+				}
 
-					licenses {
-						license {
-							name.set("Mozilla Public License Version 2.0")
-							url.set("https://www.mozilla.org/en-US/MPL/2.0/")
-						}
-					}
-
-					developers {
-						developer {
-							id.set("gdude2002")
-							name.set("Gareth Coles")
-						}
+				developers {
+					developer {
+						id.set("gdude2002")
+						name.set("Gareth Coles")
 					}
 				}
 			}
 		}
 	}
+}
 
-	signing {
-		val signingKey = System.getenv("SIGNING_KEY")?.toString()
-		val signingPassword = System.getenv("SIGNING_KEY_PASSWORD")?.toString()
-		if (signingKey != null && signingPassword != null) {
-			useInMemoryPgpKeys(String(Base64.getDecoder().decode(signingKey)), signingPassword)
-			sign(publishing.publications["maven"])
-		}
+signing {
+	val signingKey = System.getenv("SIGNING_KEY")?.toString()
+	val signingPassword = System.getenv("SIGNING_KEY_PASSWORD")?.toString()
+	if (signingKey != null && signingPassword != null) {
+		useInMemoryPgpKeys(String(Base64.getDecoder().decode(signingKey)), signingPassword)
+		sign(publishing.publications["maven"])
 	}
 }
